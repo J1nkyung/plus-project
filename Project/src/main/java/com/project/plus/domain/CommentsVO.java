6e686cf8fe97a7f8a32ca87a779d79d5054ced18
jay <earthneedsjustice@gmail.com>
2021-04-04 16:20:02 +0900

    no message

--- Project/src/main/java/com/project/plus/domain/CommentsVO.java
+++ Project/src/main/java/com/project/plus/domain/CommentsVO.java
@@ -11,13 +11,13 @@ public class CommentsVO {
 	private int commentsNum;
 	private int memberNum;
 	private int boardNum;
-	private Date commentsRegdate;
+	private String commentsRegdate;
+	private String commentsChangedRegdate;
 	private String commentsContent;
-	
+	private String memberNickname;
+	private String memberPic;
 	
 	private int commentsClass;
 	private int commentsOrder;
 	private int commentsGroupNum;
-	
-	
 }
