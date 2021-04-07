85bb5ed0ce617b754aa73389e4598213a675ed57
earthneedsjustice <77313807+earthneedsjustice@users.noreply.github.com>
2021-04-07 11:49:43 +0900

    Update InquiryController.java
    0407 11:49

--- Project/src/main/java/com/project/plus/controller/InquiryController.java
+++ Project/src/main/java/com/project/plus/controller/InquiryController.java
@@ -1,79 +1,97 @@
 package com.project.plus.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.project.plus.domain.CriteriaAdmin;
 import com.project.plus.domain.InquiryVO;
+import com.project.plus.domain.MemberVO;
+import com.project.plus.domain.PageMakerAdmin;
 import com.project.plus.service.InquiryService;
 
-@Controller
-@SessionAttributes("inquiry")
-public class InquiryController {
+import lombok.extern.log4j.Log4j;
 
+@Log4j
+@Controller
+public class InquiryController {
+	
+	public static final Logger logger = LoggerFactory.getLogger(InquiryController.class);
+	
 	@Autowired
 	private InquiryService is;
 	
-	// return 어디로 할지가 아직 정리가 안됨
+	// 회원 : 1:1 문의글 작성 폼
+	@RequestMapping(value = "/inquiryForm", method = RequestMethod.GET)
+	public String inquiryForm(InquiryVO vo, MemberVO mvo, HttpSession session) throws Exception {
+		MemberVO user = (MemberVO) session.getAttribute("user");
+		vo.setMemberNum(user.getMemberNum());
+		is.inquiryForm(vo);
+		System.out.println("inquiryForm" + vo);
+		return "inquiryForm.inqu";
+	}
 
-	@RequestMapping(value="/writeInquiry.do", method=RequestMethod.POST)
+	// 회원 : 1:1 문의글 제출 메서드
+	@RequestMapping(value = "/writeInquiry", method = RequestMethod.POST)
 	public String writeInquiry(InquiryVO vo) throws Exception {
-			is.writeInquiry(vo);
-			return "inquiry.do";
+		is.writeInquiry(vo);
+		return "redirect:inquiry";
 	}
 
-	@RequestMapping(value="/editInquiry.do", method=RequestMethod.POST)
-	public String editInquiry(@ModelAttribute("inquiry")InquiryVO vo) {
+	// 회원 : 1:1 문의글 수정 폼
+	@RequestMapping("/editInquiryForm")
+	public String editInquiryForm(InquiryVO vo, Model model) {
+		model.addAttribute("inquiryForm", is.editInquiryForm(vo));
+		return "inquiryEditForm.inqu";
+	}
+
+	// 회원 : 1:1 문의글 수정 등록 메서드
+	@RequestMapping(value = "/editInquiry", method = RequestMethod.POST)
+	public String editInquiry(InquiryVO vo) {
 		is.editInquiry(vo);
-		return "inquiry/inquiryForm";
+		return "redirect:inquiry";
 	}
-	
-//	@RequestMapping(value="/answerInquiry.do", method=RequestMethod.POST)
-//	public String answerInquiry(@ModelAttribute("inquiry")InquiryVO vo) {
-//		is.answerInquiry(vo);
-//		return "inquiry.do";
-//	}
 
-	@RequestMapping("/deleteInquiry.do")
+	// 회원 : 1:1 문의글 삭제 메서드
+	@RequestMapping("/deleteInquiry")
 	public String deleteInquiry(InquiryVO vo) {
 		is.deleteInquiry(vo);
-		return "inquiry.do";
+		return "redirect:inquiry";
 	}
 
-//	@RequestMapping(value="/getInquiry.do", method=RequestMethod.GET)
-//	public String getInquiry(InquiryVO vo, Model model) {
-//		model.addAttribute("inquiry", is.getInquiry(vo));
-//		return "inquiry/getInquiry";
-//	}
-	
-	@GetMapping("/getInquiry.do")
+	// 회원 : 1:1 문의글 개별 조회  
+	@GetMapping("/getInquiry")
 	public String getInquiry(InquiryVO vo, Model model) {
 		model.addAttribute("inquiry", is.getInquiry(vo));
-		return "inquiry/getInquiry";
+		return "getInquiry.inqu";
 	}
 	
-	@RequestMapping("/inquiry.do")
-	public String getInquiryList(InquiryVO vo, Model model) {
-		model.addAttribute("inquiryList", is.getInquiryList(vo));
-		return "inquiry.mypage";
-	}
-	
-	@RequestMapping("/inquiryPerson.do")
-	public String getInquiryPersonList(InquiryVO vo, Model model) {
-		model.addAttribute("inquiryPersonList", is.getInquiryPersonList(vo));
-		return "inquiry";
+	// 회원 : 1:1 문의 목록 조회 페이징처리 완
+	@RequestMapping(value="/inquiry", method=RequestMethod.GET)
+	public String getInquiryList(Model model, HttpSession session, CriteriaAdmin cri) throws Exception {
+		MemberVO user = (MemberVO) session.getAttribute("user");
+		
+		cri.setMemberNum(user.getMemberNum());
+		
+		List<InquiryVO> inquiryList = is.getInquiryList(cri);
+		System.out.println(inquiryList);
+		model.addAttribute("inquiryList", inquiryList);
+		
+		PageMakerAdmin pgmad = new PageMakerAdmin();
+		pgmad.setCri(cri);
+		pgmad.setTotalCount(is.inquiryListCount(cri));
+		model.addAttribute("pageMakerAdmin", pgmad);
+		
+		return "inquiry.inqu";
 	}
 
-	@RequestMapping("/inquiryType.do")
-	public String getInquiryTypeList(InquiryVO vo, Model model) {
-		model.addAttribute("inquiryTypeList", is.getInquiryTypeList(vo));
-		return "inquiry"; // 1:1문의 리스트에 뿌려주는 것은 똑같기 때문에 여기다 돌려주는거 맞겠지..?
-	}
 }
-
-
